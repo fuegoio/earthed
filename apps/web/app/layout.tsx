5db66e1a2cd5eedb359c4c68e32118bd9152bd76
Alexis Tacnet <alexis@mistral.ai>
2026-08-14 11:52:34 +0200

    update typeset

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,37 +1,32 @@
-import { Geist, Geist_Mono, Merriweather, IBM_Plex_Sans } from "next/font/google"
+import { Geist_Mono, Merriweather, IBM_Plex_Sans } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import "@workspace/ui/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@workspace/ui/lib/utils";
 
-const ibmPlexSansHeading = IBM_Plex_Sans({subsets:['latin'],variable:'--font-heading'});
+const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
-const merriweather = Merriweather({subsets:['latin'],variable:'--font-serif'});
+const merriweather = Merriweather({ subsets: ["latin"], variable: "--font-serif" });
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
+const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontSans.variable, fontMono.variable, "font-serif", merriweather.variable, ibmPlexSansHeading.variable)}
+      className={cn("antialiased", ibmPlexSans.variable, geistMono.variable, merriweather.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
